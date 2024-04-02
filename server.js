babe9710c3ac397f128583b736b901dcc50d2967
bosirex <dennis.dancan@gmail.com>
2024-04-02 13:17:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import routes from './routes/index';
+import controllerRouting from './routes/index';
 
 const app = express();
-app.use(express.json());
 const port = process.env.PORT || 5000;
 
-app.use('/', routes);
+app.use(express.json());
+
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export default app;
